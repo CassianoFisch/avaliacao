611f315a43b594fed881e65358606b3a86e03c84
CassianoFisch <cassiano.fisch@duque.g12.br>
2024-10-07 21:04:44 -0300

    final da aplicacao

---
+++ src/components/SearchForm.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+interface SearchFormProps {
+  onSearch: (query: string) => void;
+}
+
+const SearchForm: React.FC<SearchFormProps> = ({ onSearch }) => {
+  const [query, setQuery] = useState('');
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();  // Corrigi o preventDefault
+    onSearch(query);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        placeholder="Digite o nome do repositório"
+      />
+      <button type="submit">Pesquisar</button>
+    </form>
+  );
+  };
+  
+  export default SearchForm;
+  
